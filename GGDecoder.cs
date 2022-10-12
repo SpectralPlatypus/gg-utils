9592a0a0ce92d1724084d22ff3ccd0e411c3c36e
Spectral Platypus <spectralplat@protonmail.com>
2022-10-11 23:11:51 -0400

    Major optimization on decoder loops :clown_face:

--- GGDecoder.cs
+++ GGDecoder.cs
@@ -9,11 +9,14 @@ namespace GGUtils
 		public static Span<byte> GGPackDecode(Span<byte> buffer)
 		{
 			int key_idx = (buffer.Length + 0x78) & 0xFFFF;
-			for(int i = 0; i < buffer.Length; ++i)
+			var key1 = Resources.key1;
+			var key2 = Resources.key2;
+
+			for (int i = 0; i < buffer.Length; ++i)
 			{
 				byte data = buffer[i];
-				byte x = (byte)(data ^ Resources.key1[(key_idx + 0x78) & 0xFF] ^ Resources.key2[key_idx]);
-				key_idx = (key_idx + (Resources.key1[key_idx & 0xFF] & 0xFF)) & 0xFFFF;
+				byte x = (byte)(data ^ key1[(key_idx + 0x78) & 0xFF] ^ key2[key_idx]);
+				key_idx = (key_idx + (key1[key_idx & 0xFF] & 0xFF)) & 0xFFFF;
 				buffer[i] = x;
 			}
 
@@ -23,14 +26,16 @@ namespace GGUtils
 		public static Span<byte> SaveDecode(Span<byte> buffer)
 		{ 
 			uint idx = (uint)buffer.Length + 0x6c53a24a;
+			var saveKey = Resources.saveKey;
+			var key2 = Resources.key2;
 
-			for(int i = 0; i < Resources.saveKey.Length;)
-				idx ^= Resources.saveKey[i++] + (idx << 5) + (idx >> 2);
+			for(int i = 0; i < saveKey.Length;)
+				idx ^= saveKey[i++] + (idx << 5) + (idx >> 2);
 
 			for (int i = 0; i < buffer.Length; ++i)
 			{
 				uint y = ((uint)i + 0x1664 + idx) & 0xFFFF;
-                buffer[i] = (byte)(Resources.key2[y] ^ buffer[i]);
+                buffer[i] = (byte)(key2[y] ^ buffer[i]);
 			}
 
 			return buffer;
